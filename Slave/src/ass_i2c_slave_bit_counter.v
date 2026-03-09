35c1af35c551c0ba4bcb2de50b4b277f96a079fd
LeeMiRyeong <117430814+ddddddddggod@users.noreply.github.com>
2026-03-09 11:03:46 +0900

    Create ass_i2c_slave_bit_counter.v

---
+++ Slave/src/ass_i2c_slave_bit_counter.v
@@ -0,0 +1,23 @@
+module ass_i2c_slave_bit_counter(
+    input clk,
+    input rstb,
+    input count_clr,           
+    input count_inc,            
+    output count_done     
+);
+
+    reg [2:0] cnt_reg;
+
+    always @(posedge clk or negedge rstb) begin
+        if (!rstb) begin
+            cnt_reg <= 3'd0;
+        end else if (count_clr) begin
+            cnt_reg <= 3'd0;
+        end else if (count_inc) begin
+            cnt_reg <= cnt_reg + 1'b1;
+        end
+    end
+
+    assign count_done = (cnt_reg == 3'd7);
+
+endmodule
