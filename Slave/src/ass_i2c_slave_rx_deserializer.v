02d8731d9ea9c95afef48c04e1be1d4ec939340d
LeeMiRyeong <117430814+ddddddddggod@users.noreply.github.com>
2026-03-09 11:03:23 +0900

    Create ass_i2c_slave_rx_deserializer.v

---
+++ Slave/src/ass_i2c_slave_rx_deserializer.v
@@ -0,0 +1,44 @@
+module ass_i2c_slave_rx_deserializer (
+    input clk,
+    input rstb,
+    input sda_in,
+    input shift_rx_en,
+    input shift_tx_en,
+    input load_data,
+    input load_addr,
+    input inc_addr,
+    input [7:0] rdata,
+    output [7:0] wdata,
+    output reg [6:0] addr,
+    output sda_out_bit
+);
+    reg [7:0] shift_reg;
+
+    // 1. Shift Register
+    always @(posedge clk or negedge rstb) begin
+        if (!rstb) begin
+            shift_reg <= 8'h00;
+        end else if (load_data) begin
+            shift_reg <= rdata;
+        end else if (shift_rx_en) begin //write
+            shift_reg <= {shift_reg[6:0], sda_in};
+        end else if (shift_tx_en) begin //read
+            shift_reg <= {shift_reg[6:0], 1'b0};
+        end
+    end
+
+    assign wdata = shift_reg;
+    assign sda_out_bit = shift_reg[7];
+
+    // 2. Address
+    always @(posedge clk or negedge rstb) begin
+        if (!rstb) begin
+            addr <= 7'd0;
+        end else if (load_addr) begin
+            addr <= shift_reg[6:0];
+        end else if (inc_addr) begin
+            addr <= addr + 1'b1;
+        end
+    end
+
+endmodule
