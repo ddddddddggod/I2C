5c356ca60513baa3fc417c889cc41e2df74423d1
LeeMiRyeong <117430814+ddddddddggod@users.noreply.github.com>
2026-03-15 22:08:37 +0900

    Create ass_i2c_slave_rx_pkt_ctrl.v

---
+++ Slave_ver4/src/ass_i2c_slave_rx_pkt_ctrl.v
@@ -0,0 +1,54 @@
+module ass_i2c_slave_rx_pkt_ctrl (
+    input clk, 
+    input rstb,
+    input init,
+    input rdy,
+    input request,
+    input full,               // [ADDED]
+    input overflow_stop_en,   // [ADDED]
+    output we,
+    output load_addr,
+    output inc_addr,
+    output load_data
+);
+
+localparam [1:0] pkt_idle = 2'd0;
+localparam [1:0] pkt_addr = 2'd1;
+localparam [1:0] pkt_data = 2'd2;
+
+reg [1:0] pkt_state, pkt_state_n;
+
+//next state
+always @(posedge clk or negedge rstb) begin
+    if (!rstb) begin
+        pkt_state <= pkt_idle;
+    end else if (init) begin
+        pkt_state <= pkt_idle;
+    end else begin           
+        pkt_state <= pkt_state_n;
+    end
+end
+
+//current state
+always @(*) begin
+    pkt_state_n = pkt_state;
+    case (pkt_state)
+        pkt_idle: begin
+            if (rdy) begin
+                pkt_state_n = pkt_addr;
+            end else if (request) begin
+                pkt_state_n = pkt_data;
+            end
+        end
+        pkt_addr: if (rdy) pkt_state_n = pkt_data;
+        pkt_data: pkt_state_n = pkt_data;
+    endcase
+end
+
+//output logic
+assign load_addr = (pkt_state == pkt_addr) && rdy;
+assign we = (pkt_state == pkt_data) && rdy && !(overflow_stop_en && full);  //[Modified]
+assign load_data = ((pkt_state == pkt_idle) && request) || ((pkt_state == pkt_data) && request);
+assign inc_addr = ((pkt_state == pkt_idle) && request) ||  ((pkt_state == pkt_data) && (rdy || request) && !(overflow_stop_en && full)); //[Modified]
+
+endmodule
