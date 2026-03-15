337106ef699bf5c2dada09f556ab62a1a739b2bf
LeeMiRyeong <117430814+ddddddddggod@users.noreply.github.com>
2026-03-15 22:07:09 +0900

    Create ass_i2c_slave_rf.v

---
+++ Slave_ver4/src/ass_i2c_slave_rf.v
@@ -0,0 +1,39 @@
+module ass_i2c_slave_rf (
+    input clk,
+    input rstb, 
+    input we,
+    input [6:0] addr,
+    input [7:0] wdata,
+    input init,
+    output [7:0] rdata,
+    output full
+);
+    reg [7:0] mem [0:127]; // 128x8
+
+  //for optional mem_buf
+    reg full_reg;  
+    always @(posedge clk or negedge rstb) begin
+        if (!rstb) begin
+            full_reg <= 1'b0;
+        end else if (init) begin
+            full_reg <= 1'b0;
+        end else if (we && (addr == 7'd127)) begin
+            full_reg <= 1'b1;
+        end
+    end
+    assign full = full_reg;
+
+    //Write
+    integer i;
+    always @(posedge clk or negedge rstb) begin
+        if (!rstb) begin
+            for (i=0; i<128; i=i+1) mem[i] <= 8'h00;
+        end else if (we) begin  //we=1 
+            mem[addr] <= wdata;
+        end
+    end
+
+    //Read
+    assign rdata = mem[addr];
+
+endmodule
